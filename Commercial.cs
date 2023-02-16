a5268320354551374745afd557519b3c0317a91d
Adame-Qodad <adameqodad341@gmail.com>
2023-02-16 09:31:54 +0100

    Ajoutez des fichiers projet.

---
+++ Commercial.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPGestionCommercial
+{
+    internal class Commercial
+    {
+        private string nom;
+        private string prenom;
+        private int anneeNaissance;
+        private Categorie cat;
+        private Service service;
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public int AnneeNaissance { get => anneeNaissance; set => anneeNaissance = value; }
+        internal Categorie Cat { get => cat; set => cat = value; }
+        internal Service Service { get => service; set => service = value; }
+
+        public Commercial(string nom, string prenom, int anneeNaissance, Categorie cat, Service service)
+        {
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.AnneeNaissance = anneeNaissance;
+            this.Cat = cat;
+            this.Service = service;
+            this.Service.ajouterCom(this);
+        }
+
+        public int CalculAge()
+        {
+           int diff = DateTime.Now.Year - this.AnneeNaissance;
+            return diff;
+        }
+
+        public override string ToString()
+        {
+            return "nom : " + this.Nom + "\nprenom : " + this.Prenom + "\nAnnée de Naissance : " + this.AnneeNaissance + "\nCe commercial a " + CalculAge() + " ans.\n" + this.Cat + this.Service;
+        }
+
+        public void Compare(Commercial c)
+        {
+            if (this.Nom == c.Nom) 
+            {
+                Console.WriteLine("Les commerciaux "+ this.Prenom + " et " + c.Prenom + " ont le même nom de famille.\n");
+            }
+
+            else
+            {
+                Console.WriteLine("Les commerciaux "+ this.Prenom + " et " + c.Prenom + " n'ont pas le même nom de famille.\n\n");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPGestionCommercial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Categorie cat1 = new Categorie("C012", "Négociateur");
+            Categorie cat2 = new Categorie("C013", "Redistributeur");
+
+            Service s1 = new Service("National");
+            Service s2 = new Service("International");
+
+            Commercial com1 = new Commercial("Qodad", "Adame", 1997, cat1, s2);
+            Commercial com2 = new Commercial("Qodad", "Mohamed", 1969, cat2, s2);
+            Commercial com3 = new Commercial("Zahir", "Ilyes", 1984, cat1, s2);
+
+            Commercial com4 = new Commercial("El", "Jahid", 1982, cat1, s1);
+            Commercial com5 = new Commercial("Costo", "John", 1962, cat2, s1);
+            Commercial com6 = new Commercial("Dumur", "Ferkan", 1978, cat1, s1);
+
+
+
+            s1.afficheCom();
+            s2.afficheCom();
+
+            com1.Compare(com2);
+            com1.Compare(com3);
+
+            s1.plusAge();
+            s2.plusAge();
+
+        }
+
+    }
+
+}
